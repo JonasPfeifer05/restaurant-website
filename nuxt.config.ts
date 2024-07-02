e4f8b102920d8bfaae754999b2c94c91c46d1bf8
jonas <jonaspfeifer@drei.at>
2024-07-03 01:07:27 +0200

    disabling ssr to reduce memory usage on build causing crashes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: false },
-  modules: ['@pinia/nuxt', '@nuxtjs/i18n', '@nuxt/fonts', '@nuxt/icon'],
-  css: ['~/assets/css/main.scss'],
-  icon: {
-    size: '64px'
-  }
+    ssr: false,
+    devtools: {enabled: false},
+    modules: ['@pinia/nuxt', '@nuxtjs/i18n', '@nuxt/fonts', '@nuxt/icon'],
+    css: ['~/assets/css/main.scss'],
+    icon: {
+        size: '64px'
+    }
 })
