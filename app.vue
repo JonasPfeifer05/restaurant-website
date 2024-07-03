3a65eee32d6ef4e098f0d3b0c72eb685f924c228
Jonas Pfeifer <jonas.pfeifer@cloudflight.io>
2024-07-03 10:25:23 +0200

    basic slide mechanic

--- app.vue
+++ app.vue
@@ -1,12 +1,48 @@
 <script setup lang="ts">
+const settingsOpened = ref(false)
+
+function settings () {
+  settingsOpened.value = !settingsOpened.value
+}
 </script>
 
 <template>
-  <div style="display: contents">
-    <NavigationBar />
-    <LandingPage />
+  <div id="app-viewport">
+    <button style="position: absolute; z-index: 1" @click="settings">
+      Settings
+    </button>
+    <div id="app-content" :class="{ 'open-settings': settingsOpened }">
+      <div id="virtual-body">
+        <NavigationBar />
+        <LandingPage />
+      </div>
+      <div id="settings-menu" />
+    </div>
   </div>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
+#app-viewport {
+  overflow-x: hidden;
+
+  #app-content {
+    display: flex;
+
+    transition: transform 1s;
+
+    &.open-settings {
+      transform: translateX(-70vw);
+    }
+
+    #virtual-body {
+      display: block;
+    }
+
+    #settings-menu {
+      min-width: 70vw;
+      height: 100dvh;
+      background-color: red;
+    }
+  }
+}
 </style>
